736564dadd5e1b1cd45a183c5b30ff37079f5acd
K7! <phoenex659@gmail.com>
2026-02-16 16:20:52 +0530

    V3

--- src/js/background.js
+++ src/js/background.js
@@ -1,6 +1,12 @@
-chrome.runtime.onMessage.addListener((message, sender)=> {
-if(!message.isActivated) return;
-else{chrome.tabs.executeScript({
-file: 'src/js/inject.js'
-}); }
+chrome.runtime.onMessage.addListener(async (message, sender) => {
+  if(!message.isActivated) return;
+  
+  // Get the active tab
+  const [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+  
+  // Execute script using Manifest V3 API
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    files: ['src/js/inject.js']
+  });
 });
